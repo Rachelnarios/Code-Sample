e42bd98407e2bfc359c07cf2a64c7e91aabc9fdf
Rachel Rios <rnr266@nyu.edu>
2017-09-18 22:28:34 -0400

    Update e2.c
    Edited j to be 7

--- r02/e2.c
+++ r02/e2.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
     // FIXME: Modify the values of i and j so that the output in the end is -28.
     int i = 0;
-    int j = 0;
+    int j = 7;
 
     // DO NOT modify following lines.
     float array[] = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7};
@@ -14,3 +14,4 @@ int main(int argc, char **argv)
 
     printf("%d\n", (p1 - p2));
 }
+
